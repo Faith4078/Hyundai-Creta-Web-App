fb5e4e0bfc708206065d0db1b6f188e07f59f3cc
Muhammad Arif Raza <102687790+arifch786@users.noreply.github.com>
2025-12-24 17:27:07 +0500

    done

--- app/(users dashboard)/dashboard/winner-page/page.tsx
+++ app/(users dashboard)/dashboard/winner-page/page.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { auth } from '@/auth';
 import { headers } from 'next/headers';
 import { redirect } from 'next/navigation';
+
 export default async function WinnerPage() {
   // const session = await auth.api.getSession({
   //   headers: await headers(),
@@ -10,40 +11,10 @@ export default async function WinnerPage() {
   // if (!session) {
   //   redirect('/sign-in');
   // }
+
   return (
-    // <main className=" w-full min-h-[200vh] ">
-    //   <section className="max-w-[1296px] bg-gradient-to-br from-[#0A0A0A] via-[#111827] to-[#000] w-full  mx-auto px-4">
-    //     <div className="mx-auto flex flex-col items-center gap-y-[42px] mt-[37px] lg:mt-[153px]">
-    //       <Image
-    //         src={'/assets/trophy.svg'}
-    //         width={628}
-    //         height={256}
-    //         alt="hyundai creta car image"
-    //         className=""
-    //       />
-    //       <h1 className="font-inter text-white text-center font-bold text-[3.45638rem] leading-[1.81069rem] lg:text-[6rem] lg:leading-[3.14319rem]">
-    //         مبـروك!
-    //       </h1>
-    //       <p className="font-inter font-bold text-center text-white text-[1.62963rem] leading-[1.81069rem] lg:text-[2.82888rem] lg:leading-[3.14319rem]">
-    //         لقد فزت بالجائزة الكبرى!
-    //       </p>
-    //     </div>
-    //     <Image
-    //       src={'/assets/winner-car.png'}
-    //       width={1296}
-    //       height={613}
-    //       alt="hyundai creta car image"
-    //       className="my-[37px] w-full lg:my-[72px]"
-    //     />
-    //     <div className="mx-auto flex flex-col gap-y-[36px] items-center lg:mb-[126px]">
-    //       <h4 className="text-white font-inter text-center font-bold text-[1.50088rem] leading-[2.09669rem] lg:text-[2.25rem] lg:leading-[3.14319rem]">
-    //         شارك فرحتك مع العالم!
-    //       </h4>
-    //     </div>
-    //   </section>
-    // </main>
-    <main className="w-full min-h-screen">
-      <section className="max-w-[1296px] bg-gradient-to-br from-[#0A0A0A] via-[#111827] to-[#000] w-full min-h-screen mx-auto px-4">
+    <main className="w-full min-h-screen bg-gradient-to-br from-[#0A0A0A] via-[#111827] to-[#000]">
+      <section className="max-w-[1296px] w-full min-h-screen mx-auto px-4">
         <div className="mx-auto flex flex-col items-center gap-y-[42px] pt-[37px] lg:pt-[153px]">
           <Image
             src={'/assets/trophy.svg'}
@@ -60,7 +31,7 @@ export default async function WinnerPage() {
             className="size-[106px] block lg:hidden"
           />
           <h1 className="font-inter text-white text-center font-bold text-[3.45638rem] leading-[1.81069rem] lg:text-[6rem] lg:leading-[3.14319rem]">
-            مبـروك!
+            مبروك!
           </h1>
           <p className="font-inter font-bold text-center text-white text-[1.62963rem] leading-[1.81069rem] lg:text-[2.82888rem] lg:leading-[3.14319rem]">
             لقد فزت بالجائزة الكبرى!
@@ -81,7 +52,7 @@ export default async function WinnerPage() {
             type="button"
             className="bg-gradient-to-r from-[#3B82F6] to-[#0FF] w-[334px] h-[46px] rounded-[10px] text-[#0F172A] text-center flex items-center justify-center font-extrabold  text-[0.83869rem] leading-[1.258rem] lg:w-[501px] lg:h-[70px] lg:rounded-[15px]  lg:text-[1.25731rem]  lg:leading-[6rem] hover:cursor-pointer"
           >
-            شارك على إنستغرام
+            شارك عبر إنستغرام
           </button>
         </div>
       </section>
